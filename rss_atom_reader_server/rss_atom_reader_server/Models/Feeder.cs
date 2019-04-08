a58600ea7312e94f375211f7dfe39788049970a8
Michał Babik <michalbabik1995@gmail.com>
2019-04-08 17:34:00 +0200

    Create Feeder class

---
+++ rss_atom_reader_server/rss_atom_reader_server/Models/Feeder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rss_atom_reader_server.Models
+{
+    public class Feeder
+    {
+
+    }
+}
