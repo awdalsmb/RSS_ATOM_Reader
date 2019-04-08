8003311790e96748c94a2e038702020f70f9e426
Michał Babik <michalbabik1995@gmail.com>
2019-04-08 22:07:32 +0200

    Complete the Feeder class

--- rss_atom_reader_server/rss_atom_reader_server/Models/Feeder.cs
+++ rss_atom_reader_server/rss_atom_reader_server/Models/Feeder.cs
@@ -7,6 +7,10 @@ namespace rss_atom_reader_server.Models
 {
     public class Feeder
     {
-
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string PublishDate { get; set; }
+        public string Description { get; set; }
     }
 }
