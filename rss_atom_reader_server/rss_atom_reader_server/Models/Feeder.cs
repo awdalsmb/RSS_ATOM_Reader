0ced1d76c621d056cacfb128d1b6418600a614b0
Michał Babik <michalbabik1995@gmail.com>
2019-04-10 19:56:52 +0200

    Update Feeder class

--- rss_atom_reader_server/rss_atom_reader_server/Models/Feeder.cs
+++ rss_atom_reader_server/rss_atom_reader_server/Models/Feeder.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace rss_atom_reader_server.Models
 {
     public class Feeder
     {
-        public int Id { get; set; }
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonRequired]
         public string Title { get; set; }
         public string Link { get; set; }
-        public string PublishDate { get; set; }
+        public DateTime PublishDate { get; set; }
         public string Description { get; set; }
     }
 }
