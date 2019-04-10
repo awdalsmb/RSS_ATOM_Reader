0900b34a87b2e1470b58acc2e1ebb333e55810d9
Michał Babik <michalbabik1995@gmail.com>
2019-04-10 21:40:47 +0200

    Create DbService class

---
+++ rss_atom_reader_server/rss_atom_reader_server/Services/DbService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using rss_atom_reader_server.Models;
+
+namespace rss_atom_reader_server.Services
+{
+    public class DbService
+    {
+        private IMongoCollection<Feeder> FeederCollection { get; }
+
+        public DbService(string databaseName, string collectionName, string databaseUrl)
+        {
+            var mongoClient = new MongoClient(databaseUrl);
+            var mongoDatabase = mongoClient.GetDatabase(databaseName);
+
+            FeederCollection = mongoDatabase.GetCollection<Feeder>(collectionName);
+        }
+
+        public async Task<List<Feeder>> GetAllFeeds()
+        {
+            var feeds = new List<Feeder>();
+
+            var allDocuments = await FeederCollection.FindAsync(new BsonDocument);
+            await allDocuments.ForEachAsync(doc => feeds.Add(doc));
+
+            return feeds;
+
+        }
+    }
+}
