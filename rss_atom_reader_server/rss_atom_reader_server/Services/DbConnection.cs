c4662c416d661a860daebd01cd196cd8215bc1b6
Michał Babik <michalbabik1995@gmail.com>
2019-04-10 20:08:05 +0200

    Update Feeder class

---
+++ rss_atom_reader_server/rss_atom_reader_server/Services/DbConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rss_atom_reader_server.Services
+{
+    public class DbConnection
+    {
+
+    }
+}
