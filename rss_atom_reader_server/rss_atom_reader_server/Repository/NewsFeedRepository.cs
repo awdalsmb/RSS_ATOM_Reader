f438aa55baf0736f1c79e5928e1569fc9d7827bc
Michał Babik <michalbabik1995@gmail.com>
2019-04-12 16:45:24 +0200

    Create NewsFeedRepository

---
+++ rss_atom_reader_server/rss_atom_reader_server/Repository/NewsFeedRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rss_atom_reader_server.Repository
+{
+    public class NewsFeedRepository
+    {
+    }
+}
