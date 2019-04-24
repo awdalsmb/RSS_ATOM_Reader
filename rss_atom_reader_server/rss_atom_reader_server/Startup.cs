3723e45ce74cb77f61152113af791bd87f4cd538
Michał Babik <michalbabik1995@gmail.com>
2019-04-24 19:43:13 +0200

    Setting up authentication in Startup

--- rss_atom_reader_server/rss_atom_reader_server/Startup.cs
+++ rss_atom_reader_server/rss_atom_reader_server/Startup.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using rss_atom_reader_server.DataBaseModels;
 using rss_atom_reader_server.IRepository;
 using rss_atom_reader_server.Repository;
+using System.Text;
 
 namespace rss_atom_reader_server
 {
@@ -20,9 +23,26 @@ namespace rss_atom_reader_server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string securityKey = "this_is_our_supper_long_security_key_for_token_validation_for_rss_atom_reader_2019$smesk.in";
+
+           var symetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
             services.AddMvc();
             services.Configure<Settings>(o => { o.iConfigurationRoot = Configuration; });
             services.AddTransient<INewsFeedRepository, NewsFeedRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "smesk.in",
+                        ValidAudience = "readers",
+                        IssuerSigningKey = symetricSecurityKey
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
